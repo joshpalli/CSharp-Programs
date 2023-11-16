824cc1359a361e844316fc1011c3ca19613ab3a7
joshpalli <149811427+joshpalli@users.noreply.github.com>
2023-11-16 13:34:39 -0600

    Add files via upload

---
+++ ContainsDuplicates.cs
@@ -0,0 +1,29 @@
+using System;
+
+// This class takes in an integer array, and returns a boolean if it contains duplicate numbers.
+public class ContainsDuplicates
+{
+    // Create a method to detect if array has duplicates.
+    public static bool duplicate(int[] nums)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j]) 
+                {
+                    Console.WriteLine("This Array has duplicates!!");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Make a Driver method to test function.
+    public static void Main(string[] args)
+    {
+        int[] nums = {3, 2, 3, 4};
+        duplicate(nums);
+    }
+}
---
+++ HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+
+    public class HelloWorld
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I am Joshua Palli!");
+            Console.ReadKey();
+        }
+    }
+}
